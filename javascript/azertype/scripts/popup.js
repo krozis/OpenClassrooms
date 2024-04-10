4e725a29e1def6ce529419ff00c0f92f41925966
krozis <stelie@student.42.fr>
2024-04-10 14:50:04 +0200

    validation rules exercice done

--- javascript/azertype/scripts/popup.js
+++ javascript/azertype/scripts/popup.js
@@ -1,4 +1,4 @@
-	function	displayPopup()
+function	displayPopup()
 {
 	let	popup = document.querySelector(".popupBackground")
 
@@ -20,6 +20,18 @@ function	displayMail(name, mail)
     location.href = mailto
 }
 
+function	checkName(name)
+{
+	let	regex = new RegExp("[a-zA-Z]{2,}"); //minimum 2 letters (upper/lower accepted);
+	return (regex.test(name));
+}
+
+function	checkMail(mail)
+{
+	let	regex = new RegExp("[a-z0-9._-]+@[a-z0-9._-]+\.[a-z0-9._-]+");
+	return (regex.test(mail));
+}
+
 function	initPopup()
 {
 	// prevent default event on the form
@@ -39,20 +51,41 @@ function	initPopup()
 	
 	// Click on close button
 	let	closeBtn = document.getElementById("close");
-	closeBtn.addEventListener("click", () =>
+	closeBtn.addEventListener("click", (event) =>
 	{
+			event.preventDefault();
 			hidePopup();
 	});
 
 	// Click on the "Envoyer" button
-	let	sendBtn = document.getElementById("btnEnvoyerMail");
-	sendBtn.addEventListener("click", () =>
+	let	sendBtn = document.getElementById("sendMailForm");
+	sendBtn.addEventListener("submit", (event) =>
 	{
 		let	nameInput = document.getElementById("nom");
 		let	name = nameInput.value;
 		let	mailInput = document.getElementById("email");
 		let mail = mailInput.value;
-		displayMail(name, mail);
-		console.log(name, mail);
+	
+		if (!checkName(name))
+		{
+			console.log("Error, invalid name");
+			nameInput.classList.add("error");
+			nameInput.classList.remove("correct");
+		}
+		else if (!checkMail(mail))
+		{
+			console.log("Error, invalid mail");
+			mailInput.classList.add("error");
+			mailInput.classList.remove("correct");
+		}
+		else
+		{
+			nameInput.classList.remove("error");
+			nameInput.classList.add("correct");
+			mailInput.classList.remove("error");
+			mailInput.classList.add("correct");
+			displayMail(name, mail);
+			console.log(name, mail);
+		}
 	})
 }
